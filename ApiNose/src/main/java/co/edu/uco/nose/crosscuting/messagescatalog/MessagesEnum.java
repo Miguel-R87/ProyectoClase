3902bf822ebb553803bc85cefaf5a5a2b1168a63
Miguel <Miguel@DESKTOP-S1A1P38>
2025-10-09 23:34:23 -0500

    constructores de los DAO

--- ApiNose/src/main/java/co/edu/uco/nose/crosscuting/messagescatalog/MessagesEnum.java
+++ ApiNose/src/main/java/co/edu/uco/nose/crosscuting/messagescatalog/MessagesEnum.java
@@ -6,9 +6,16 @@ public enum MessagesEnum {
 	
 USER_ERROR_SQL_CONNECTION_IS_EMPTY("Conexión contra la fuente de información deseada vacia","La conexión requerida para llevar a cabo la operacion contra la fuente de información deseada esta vacia.Por favor intente de nuevo y si el problema persiste contacte al administrador de la aplicación."),
 
-TECHNICAL_ERROR_SQL_CONNECTION_IS_EMPTY("Conexión contra la fuente de información deseada nula","La conexión requerida para llevar a cabo la operacion contra la base de datos llego nula");
+TECHNICAL_ERROR_SQL_CONNECTION_IS_EMPTY("Conexión contra la fuente de información deseada nula","La conexión requerida para llevar a cabo la operacion contra la base de datos llego nula"),
 
 
+USER_ERROR_SQL_CONNECTION_IS_CLOSED("Conexión contra la fuente de información deseada cerrada","La conexión requerida para llevar a cabo la operacion contra la fuente de información deseada esta cerrada.Por favor intente de nuevo y si el problema persiste contacte al administrador de la aplicación."),
+
+TECHNICAL_ERROR_SQL_CONNECTION_IS_CLOSED("Conexión contra la fuente de información deseada cerrada","La conexión requerida para llevar a cabo la operacion contra la base de datos esta cerrada"),
+		
+USER_ERROR_SQL_CONNECTION_UNEXCEPTED_ERROR_VALIDATING_CONNECTION_STATUS("Problema inesperado validando el estado de la conexion contra la fuente de datos deseada","Se ha presentado un problema inesperado tratando de validar el estado de la conexion requerida para llevar a cabo la operación contra la fuente de información deseada.por favor intente de nuevo y si el problema persiste contacte al administrador de la aplicación."),
+
+TECHNICAL_ERROR_SQL_CONNECTION_UNEXCEPTED_ERROR_VALIDATING_CONNECTION_STATUS("Error inesperado validando si la conexión contra la base de datos estaba abierta","Se presento un error de tipo SQLException al validar si la conexion contra la base de datos esta o no abierta,por favor valide la consola de errores para revisar con detalle el problema presentado");
 private String title;
 private String content;
 private MessagesEnum(final String title, final String content) {
--- ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/postgresql/CitySqlServerDAO.java
+++ ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/postgresql/CitySqlServerDAO.java
@@ -1,13 +1,21 @@
 package co.edu.uco.nose.data.dao.entity.postgresql;
+import java.sql.Connection;
 
 import co.edu.uco.nose.data.dao.entity.CityDAO;
+import co.edu.uco.nose.data.dao.entity.SqlConnection;
 import co.edu.uco.nose.entity.CityEntity;
 
 import java.util.UUID;
+
+
+
 import java.util.List;
 
-public final class CitySqlServerDAO implements CityDAO {
-
+public final class CitySqlServerDAO extends SqlConnection implements CityDAO {
+	
+public CitySqlServerDAO(final Connection connection) {
+		super(connection);
+	}
 	@Override
 	public List<CityEntity> findAll() {
 		// TODO Auto-generated method stub
